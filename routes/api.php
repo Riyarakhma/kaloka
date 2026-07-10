38df9733ef79d9c9d38e3c69a6e2578fcbd92fca
narazla <narazlaazzahra@gmail.com>
2026-07-10 12:58:23 +0700

    Tambah RESTful API Pengaturan (index & update, admin only)

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\KearifanLokalController;
+use App\Http\Controllers\Api\PengaturanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-// Publik: bisa diakses tanpa login
+// Publik
 Route::get('/kearifan-lokal', [KearifanLokalController::class, 'index']);
 Route::get('/kearifan-lokal/{kearifanLokal}', [KearifanLokalController::class, 'show']);
+Route::get('/pengaturan', [PengaturanController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -18,12 +20,11 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Perlu login (admin & pengelola boleh)
     Route::post('/kearifan-lokal', [KearifanLokalController::class, 'store']);
     Route::put('/kearifan-lokal/{kearifanLokal}', [KearifanLokalController::class, 'update']);
 
-    // Perlu login DAN harus admin
     Route::middleware('peran.api:admin')->group(function () {
         Route::delete('/kearifan-lokal/{kearifanLokal}', [KearifanLokalController::class, 'destroy']);
+        Route::put('/pengaturan', [PengaturanController::class, 'update']);
     });
 });
